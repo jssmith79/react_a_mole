e4d1532d9e82e0daa66808252c2190769b318052
jssmith79 <i.am.hef@gmail.com>
2023-04-11 20:46:51 -0700

    test commit

---
+++ src/components/Mole.jsx
@@ -0,0 +1,3 @@
+export default function Mole(){
+    return <h3>Mole</h3>
+}
---
+++ src/components/MoleContainer.jsx
@@ -0,0 +1,10 @@
+import Mole from "./Mole"
+
+export default function MoleContainer() {
+    return (
+        <div className="mole-conatiner">
+            <h2>Mole Container</h2>
+            <Mole />
+        </div>
+    )
+}
